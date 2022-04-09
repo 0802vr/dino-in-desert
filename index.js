4dd2147af89f4e18559aa9029a7a5dc98071a792
vaso <vasyarovnov0802@gmail.com>
2022-04-09 18:56:06 +0400

    boom

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ let isAlive =  setInterval (
     function(){
         let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
         let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){ alert ("GAME OVER - обновите страницу")}
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){ soundClick()}
+ 
     
     }, 10)
+
+    function soundClick() {
+      let audio = new Audio(); // Создаём новый элемент Audio
+      audio.src = 'boom.mp3'; // Указываем путь к звуку "клика"
+      audio.autoplay = true; // Автоматически запускаем
+    }
